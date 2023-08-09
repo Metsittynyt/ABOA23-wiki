47dbdcbf2913ceb6a662985b869a3a3b8784355d
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2023-08-09 21:11:45 +0300

    commented out for now

--- static/timeline.js
+++ static/timeline.js
@@ -1,19 +1,19 @@
-const buttons = document.querySelectorAll('.buttons label');
-const sections = document.querySelectorAll('.section');
+// const buttons = document.querySelectorAll('.buttons label');
+// const sections = document.querySelectorAll('.section');
 
-// Listen for the scroll event on the container
-document.querySelector('.timeline').addEventListener('scroll', () => {
-  // Calculate the current section index based on scroll position
-  const currentIndex = Math.round(document.querySelector('.timeline').scrollTop / window.innerHeight);
+// // Listen for the scroll event on the container
+// document.querySelector('.timeline').addEventListener('scroll', () => {
+//   // Calculate the current section index based on scroll position
+//   const currentIndex = Math.round(document.querySelector('.timeline').scrollTop / window.innerHeight);
 
-  // Update button states
-  buttons.forEach((button, index) => {
-    if (index === currentIndex) {
-      button.style.opacity = '1';
-      button.style.height = '50px';
-    } else {
-      button.style.opacity = '0.5';
-      button.style.height = '20px';
-    }
-  });
-});
+//   // Update button states
+//   buttons.forEach((button, index) => {
+//     if (index === currentIndex) {
+//       button.style.opacity = '1';
+//       button.style.height = '50px';
+//     } else {
+//       button.style.opacity = '0.5';
+//       button.style.height = '20px';
+//     }
+//   });
+// });
