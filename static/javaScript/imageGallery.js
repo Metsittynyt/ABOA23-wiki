35887942d2dacad2a7d74140700aa076e024c9ee
Elsa <elvleh@utu.fi>
2023-09-24 18:27:23 +0300

    automated slideshow to kindergarten photos

--- static/javaScript/imageGallery.js
+++ static/javaScript/imageGallery.js
@@ -24,3 +24,25 @@ if (document.querySelector('.imageGridGallery')) {
         x[slideIndex[no] - 1].style.display = "block";
     }
 }
+
+if (document.querySelector('#kindergarten_gallery')) {
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("kindergarten_photos");
+        let dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 5000); // Change image every 2 seconds
+    }
+}
