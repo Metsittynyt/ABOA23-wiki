8e52854bbe9141fd8c9a4fe9f60dc8bf8ab74c8a
Elsa <elvleh@utu.fi>
2023-09-27 12:45:56 +0300

    made another slideshow

--- static/javaScript/imageGallery.js
+++ static/javaScript/imageGallery.js
@@ -32,7 +32,29 @@ if (document.querySelector('#kindergarten_gallery')) {
     function showSlides() {
         let i;
         let slides = document.getElementsByClassName("kindergarten_photos");
-        let dots = document.getElementsByClassName("dot");
+        let dots = document.getElementsByClassName("dots1");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 5000); // Change image every 2 seconds
+    }
+}
+
+if (document.querySelector('#loved-ones-gallery')) {
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("loved-ones-lab-photos");
+        let dots = document.getElementsByClassName("dots2");
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
